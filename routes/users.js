502ae3ea2d101c583ecd97822ff5ed4cd6c497cf
ERILI <16095841d@connect.polyu.hk>
2019-01-19 22:04:57 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,47 +8,42 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('users/signup', { title: 'Sign up' });
-});
-
 router.get('/layout', function(req, res, next) {
-  res.render('/layout', { title: 'User login page' });
+  console.log("layout");
+  res.render('layout', { title: 'User login page' });
 });
 
 router.get('/signin', function(req, res, next) {
   res.render('users/signin', { title: 'Sign in' });
 });
 
+router.get('/signup', function(req, res, next) {
+  console.log("2333");
+  res.render('users/signup', { title: 'Sign up' });
+});
+
 router.post('/signin', function(req, res, next) {
+  console.log("signin post");
   if (req.body.username && req.body.password) {
     User.authenticate(req.body.username, req.body.password, function (error, user) {
       if (error) {
-        res.render('users/signin', {
-          error: true,
-          errorMsg: 'This user does not exist. Please try again. '
-        });
+        res.redirect('layout');
       } else if (!user || error) {
-        res.render('users/signin', {
-          error: true,
-          errorMsg: 'Username or password is incorrect. Please try again. '
-        });
+        res.redirect('layout');
       } else {
         res.render('users/signin', { title: 'Sign in' });
       }
     });
   } else {
-    res.render('users/signin', {
-      error: true,
-      errorMsg: 'All fields are required. Please try again. '
-    });
+    res.redirect('layout');
   }
 });
 
 router.post('/signup', function(req, res, next) {
+  console.log("gogogogo");
   User.newUser(req.body.username, req.body.password, function(err, user){
     console.log(user);
-    res.render('users/signup', { title: 'Sign up' });
+    res.redirect('layout');
   });
 });
 module.exports = router;
